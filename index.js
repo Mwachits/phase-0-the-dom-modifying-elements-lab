94d173cb53c6f2d0bba9cacf181c314a442f0e6f
David Karauni <davymwachits@gmail.com>
2024-07-24 14:03:35 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = 'YOUR-NAME is the champion';
